1f0a2d148e5c7c40c0c64b6e82a8d4404271d571
AlexKaryasov <hypotalamus95@gmail.com>
2020-09-09 20:52:20 +0400

    menu layout initial commit

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,25 @@
 let content = document.querySelector('#content')
 
+const menuItems = [];
+
+class MenuItem {
+    constructor(name, description, price) {
+        this.name = name
+        this.description = description
+        this.price = price
+    }
+}
+
 const renderMenuPage = () => {
+    let menuGridContainer = document.createElement('div')
+    menuGridContainer.classList.add('menu-content-grid-container')
+
+    content.appendChild(menuGridContainer)
     return content
+}
+
+const renderMenuItem = menuItem => {
+
 }
 
 export default renderMenuPage
